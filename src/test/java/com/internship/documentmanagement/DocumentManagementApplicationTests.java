5cddc9dbb97f8bea357bb38342ac61a118daf98a
NeRrO106 <andreilimit@yahoo.com>
2026-05-14 12:21:50 +0300

    Initial project

---
+++ src/test/java/com/internship/documentmanagement/DocumentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.documentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
