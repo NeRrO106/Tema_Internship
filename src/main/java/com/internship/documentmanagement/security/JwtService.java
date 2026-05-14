8f0ee356d25f18effb95ab9836fe51f1b20e4047
NeRrO106 <andreilimit@yahoo.com>
2026-05-14 13:35:20 +0300

    Implemented Jwt Security

---
+++ src/main/java/com/internship/documentmanagement/security/JwtService.java
@@ -0,0 +1,60 @@
+package com.internship.documentmanagement.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.Base64;
+
+@Component
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("$jwt.expiration")
+    private Long expiration;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractClaims(token).get("role", String.class);
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
---
+++ src/main/java/com/internship/documentmanagement/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.internship.documentmanagement.security;
+
+public class SecurityConfig {
+}
