9289d63694299484679d1d233b0055ae3ffa8d53
NeRrO106 <andreilimit@yahoo.com>
2026-05-18 17:06:15 +0300

    Bug fix for filehash

--- src/main/java/com/internship/documentmanagement/util/FileHashUtil.java
+++ src/main/java/com/internship/documentmanagement/util/FileHashUtil.java
@@ -1,4 +1,11 @@
 package com.internship.documentmanagement.util;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.InputStream;
+
 public class FileHashUtil {
+    public static String calculateSHA256(InputStream inputStream) throws Exception {
+        return DigestUtils.sha256Hex(inputStream);
+    }
 }
