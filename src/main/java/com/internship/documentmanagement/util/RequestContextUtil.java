141768c7c73adf2bc7e1a0ceaad7f4253032bf88
NeRrO106 <andreilimit@yahoo.com>
2026-05-18 17:07:11 +0300

    Bug fix for request context

--- src/main/java/com/internship/documentmanagement/util/RequestContextUtil.java
+++ src/main/java/com/internship/documentmanagement/util/RequestContextUtil.java
@@ -1,4 +1,27 @@
 package com.internship.documentmanagement.util;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 public class RequestContextUtil {
+    public static String getClientIpAddress() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) return null;
+
+        HttpServletRequest request = attributes.getRequest();
+        String xForwardedFor = request.getHeader("X-Forwarded-For");
+        if (xForwardedFor != null && !xForwardedFor.isEmpty()) {
+            return xForwardedFor.split(",")[0];
+        }
+        return request.getRemoteAddr();
+    }
+
+    public static String getUserAgent() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) return null;
+
+        HttpServletRequest request = attributes.getRequest();
+        return request.getHeader("User-Agent");
+    }
 }
