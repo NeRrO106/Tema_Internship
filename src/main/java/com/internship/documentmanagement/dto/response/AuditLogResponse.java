d13a35271844ba72670aff760c8f4ddcf7e26294
NeRrO106 <andreilimit@yahoo.com>
2026-05-18 16:59:20 +0300

    Updated AuditLogResponse

--- src/main/java/com/internship/documentmanagement/dto/response/AuditLogResponse.java
+++ src/main/java/com/internship/documentmanagement/dto/response/AuditLogResponse.java
@@ -1,4 +1,24 @@
 package com.internship.documentmanagement.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuditLogResponse {
+    private Long id;
+    private String username;
+    private String action;
+    private String resourceType;
+    private Long resourceId;
+    private String ipAddress;
+    private String userAgent;
+    private String reason;
+    private LocalDateTime createdAt;
 }
